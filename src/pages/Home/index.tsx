c4d480ff8969b371eec2c59c4c9fc90d73c8610b
Emerson Silva <emerson_jds@live.com>
2023-11-03 12:18:06 -0300

    adjust Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,53 @@
 import React from 'react';
 // import Loading from '../../components/Loading';
 import Logo from '../../assets/images/deco.png';
+import Logo1 from '../../assets/store_logo/torra.png';
+import Logo2 from '../../assets/store_logo/zee.png';
+import Logo3 from '../../assets/store_logo/33.png';
+import Logo4 from '../../assets/store_logo/44.png';
+import Logo5 from '../../assets/store_logo/55.png';
+import Logo6 from '../../assets/store_logo/66.png';
+
+const storeData = [
+  {
+    id: 1,
+    name: 'Torra Torra',
+    logo: Logo1,
+    cash: 10,
+  },
+  {
+    id: 2,
+    name: 'Zee Dog',
+    logo: Logo2,
+    cash: 10,
+  },
+  {
+    id: 3,
+    name: 'Lolja',
+    logo: Logo3,
+    cash: 10,
+  },
+  {
+    id: 4,
+    name: 'Uncle K',
+    logo: Logo4,
+    cash: 10,
+  },
+  {
+    id: 5,
+    name: 'Baw',
+    logo: Logo5,
+    cash: 10,
+  },
+  {
+    id: 6,
+    name: 'Osklen',
+    logo: Logo6,
+    cash: 10,
+  },
+]
+
+
 
 // import { Container } from './styles';
 
@@ -9,20 +56,19 @@ const Home: React.FC = () => {
     <>
       <div
         style={{
-
           height: '480px',
           width: '480px',
-
         }}
         className="flex flex-col"
       >
         {/* HEADER */}
+
+
         <div
           style={{
-
+            height: '20vh',
             width: '100vw',
             backgroundColor: '#1c2d34',
-
           }}
           className='flex flex-row items-center p-x-4 p-y-2'
         >
@@ -69,8 +115,10 @@ const Home: React.FC = () => {
             padding: '1rem',
           }}
         >
+          <div className='mt-2 mb-2'>
+            <p className="text-[#0A2121] text-lg ml-4">Veja algumas lojas parceiras</p>
+          </div>
 
-          <p className="text-slate-700 text-lg">Veja algumas lojas parceiras</p>
 
           <div style={{
             display: 'flex',
@@ -84,158 +132,58 @@ const Home: React.FC = () => {
           }}>
 
             {/* CARD STORE */}
-            <div
-              className='h-24 w-24 border-2 border-black bg-neutral-400 rounded'
-            >
-              <div className='h-1/4'>
-                <span>
-                  <h1>Nome Loja</h1>
-                </span>
-              </div>
 
-              <div className='h-2/4 bg-orange-600' >
-                <p>logo Loja</p>
-              </div>
+            {
+              storeData.map((store) => (
+                <div
+                  key={store.id}
+                  className='h-24 w-24 border rounded'
+                >
+                  <div className='h-1/4' style={{
+                    borderBottomWidth: '1px',
+                    borderBottomColor: '#000',
+                  }}>
+                    <span className='text-center text-slate-900'>
+                      <h1>{store.name}</h1>
+                    </span>
+                  </div>
 
-              <div className='h-1/4'>
-                <span>
-                  <h1>Nome Loja</h1>
-                </span>
-              </div>
-            </div>
-            {/* CARD STORE */}
+                  <div className='h-2/4 p-1' >
+                    <img src={store.logo} alt="Logo" className='h-full w-full object-contain' />
+                  </div>
 
-            {/* CARD STORE */}
-            <div
-              className='h-24 w-24 border-2 border-black bg-neutral-400 rounded'
-            >
-              <div className='h-1/4'>
-                <span>
-                  <h1>Nome Loja</h1>
-                </span>
-              </div>
-
-              <div className='h-2/4 bg-orange-600' >
-                <p>logo Loja</p>
-              </div>
-
-              <div className='h-1/4'>
-                <span>
-                  <h1>Nome Loja</h1>
-                </span>
-              </div>
-            </div>
-            {/* CARD STORE */}
-
-            {/* CARD STORE */}
-            <div
-              className='h-24 w-24 border-2 border-black bg-neutral-400 rounded'
-            >
-              <div className='h-1/4'>
-                <span>
-                  <h1>Nome Loja</h1>
-                </span>
-              </div>
-
-              <div className='h-2/4 bg-orange-600' >
-                <p>logo Loja</p>
-              </div>
-
-              <div className='h-1/4'>
-                <span>
-                  <h1>Nome Loja</h1>
-                </span>
-              </div>
-            </div>
-            {/* CARD STORE */}
-
-            {/* CARD STORE */}
-            <div
-              className='h-24 w-24 border-2 border-black bg-neutral-400 rounded'
-            >
-              <div className='h-1/4'>
-                <span>
-                  <h1>Nome Loja</h1>
-                </span>
-              </div>
-
-              <div className='h-2/4 bg-orange-600' >
-                <p>logo Loja</p>
-              </div>
-
-              <div className='h-1/4'>
-                <span>
-                  <h1>Nome Loja</h1>
-                </span>
-              </div>
-            </div>
-            {/* CARD STORE */}
-
-            {/* CARD STORE */}
-            <div
-              className='h-24 w-24 border-2 border-black bg-neutral-400 rounded'
-            >
-              <div className='h-1/4'>
-                <span>
-                  <h1>Nome Loja</h1>
-                </span>
-              </div>
-
-              <div className='h-2/4 bg-orange-600' >
-                <p>logo Loja</p>
-              </div>
-
-              <div className='h-1/4'>
-                <span>
-                  <h1>Nome Loja</h1>
-                </span>
-              </div>
-            </div>
-            {/* CARD STORE */}
-
-            {/* CARD STORE */}
-            <div
-              className='h-24 w-24 border-2 border-black bg-neutral-400 rounded'
-            >
-              <div className='h-1/4'>
-                <span>
-                  <h1>Nome Loja</h1>
-                </span>
-              </div>
-
-              <div className='h-2/4 bg-orange-600' >
-                <p>logo Loja</p>
-              </div>
-
-              <div className='h-1/4'>
-                <span>
-                  <h1>Nome Loja</h1>
-                </span>
-              </div>
-            </div>
-            {/* CARD STORE */}
-
-
-
-
-
-
-
-
+                  <div className='h-1/4 bg-[#02F67C] flex items-center justify-center'>
+                    <span className='text-center'>
+                      <h1 className='text-[#0A2121]'>{store.cash}% DCash</h1>
+                    </span>
+                  </div>
+                </div>
+              ))
+            }
           </div>
-
-
-          {/* LEFT SIDE */}
-          {/* <div>
-            <span>BALANCE</span>
-          </div> */}
-
-          {/* RIGHT SIDE */}
-          {/* <div>
-            <span>TRANSACTIONS</span>
-          </div> */}
         </div>
 
+        <div
+          style={{
+            height: '20vh',
+            width: '100vw',
+            padding: '1rem',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+            display: 'flex',
+            flexDirection: 'column'
+          }}
+        >
+          <button className="bg-[#02F67C] font-bold py-2 px-4 rounded w-full">
+            <span className='text-[#0A2121]'>
+              Acessar minha carteira DCash
+            </span>
+          </button>
+
+          <a href="" className='text-[#02F67C] mt-2'>
+            Ver mais ofertas
+          </a>
+        </div>
 
       </div>
     </>
